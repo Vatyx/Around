499072b5a16646146c393ae197f6b4f67c4bc802
Sahil Dhanju <sahildhanju1@gmail.com>
2016-07-09 13:20:13 -0700

    Added mock route

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -11,5 +11,23 @@ UserController.me = function(req, res, render) {
 
 };
 
+UserController.mock = function(req, res, render) {
+    console.log("User mock function");
+	var achivements = {test: "test string"};
+
+    console.log(req.user)
+    res.json(achivements);
+
+};
+
+UserController.save = function(req, res, render) {
+    console.log("User save function");
+	var achivements = {test: "save string"};
+
+    console.log(req.user)
+    res.json(achivements);
+
+};
+
 
 module.exports = UserController;
