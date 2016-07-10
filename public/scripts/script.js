1ae1ecc1f7d291968fdaf8ea8942ec119ffa24d3
Sahil Dhanju <sahildhanju1@gmail.com>
2016-07-10 08:12:09 -0700

    frontend

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,5 @@
+var currentLanguage = "";
+
 Vue.component('goal', {
   template: "#goal-template",
   props: ['goal']
@@ -6,6 +8,11 @@ Vue.component('goal', {
 Vue.component('language', {
   template: "#language-template",
   props: ['language']
+});
+
+Vue.component('achievement', {
+  template: "#achievement-template",
+  props: ['achievement']
 });
 
 var vm = new Vue({
@@ -38,6 +45,38 @@ var vm = new Vue({
 					imgsrc: "https://camo.githubusercontent.com/eb464a60a4a47f8b600aa71bfbc6aff3fe5c5392/68747470733a2f2f7261772e6769746875622e636f6d2f766f6f646f6f74696b69676f642f6c6f676f2e6a732f6d61737465722f6a732e706e67",
 					language: "Javascript",
 					progress: 80
-				}]
+				}],
+    achievement: [{
+                title: "Oh looky a title",
+                points: 15,
+				desc: "This will be more descriptive once we have actual achievements",
+            },
+            {
+                title: "Oh looky a title",
+                points: 15,
+				desc: "This will be more descriptive once we have actual achievements",
+            },
+            {
+				title: "Oh looky a title",
+				points: 15,
+				desc: "This will be more descriptive once we have actual achievements",
+            }]
   }
 });
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
+function nextPage() {
+	currentLanguage = $(this).attr("id");
+	$("#lists").addClass('animated fadeOutLeft');
+	$("#titles").addClass('animated fadeOutLeft');
+	$(".unique").show();
+	$(".unique").addClass('animated fadeInRight');
+}
